d89f4d4c67b9020ceeb3f191d7554eb7dba5e660
mrschroeder <mike.schroeder@sportngin.com>
2018-10-14 12:29:29 -0500

    more swift warnings fixed

--- Pods/EPExtensions.swift
+++ Pods/EPExtensions.swift
@@ -12,16 +12,16 @@ import Foundation
 extension String {
     subscript(r: Range<Int>) -> String? {
         get {
-            let stringCount = self.characters.count as Int
+            let stringCount = self.count as Int
             if (stringCount < r.upperBound) || (stringCount < r.lowerBound) {
                 return nil
             }
-            let startIndex = self.characters.index(self.startIndex, offsetBy: r.lowerBound)
-            let endIndex = self.characters.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
+            let startIndex = self.index(self.startIndex, offsetBy: r.lowerBound)
+            let endIndex = self.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
             return String(self[(startIndex ..< endIndex)])
         }
     }
-    
+
     func containsAlphabets() -> Bool {
         //Checks if all the characters inside the string are alphabets
         let set = CharacterSet.letters
